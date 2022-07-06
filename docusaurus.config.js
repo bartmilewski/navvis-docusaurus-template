9af61c0ff04223f531cfd4c28952cc3beebfdeee
bartmilewski <87972833+bartmilewski@users.noreply.github.com>
2022-07-06 12:43:25 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,7 +54,8 @@ module.exports = {
       },
     ],
   ],
-  plugins: //[
+  plugins: []
+            //[
            // [require.resolve('@cmfcmf/docusaurus-search-local'),{}]
            // ],
 };
